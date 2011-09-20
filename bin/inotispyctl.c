6dde010b7a5a99ed2a893b5e6262bcbe8e94c7de
James Conerly <james.conerly@gmail.com>
2011-09-20 22:28:39 +0000

    added place holder for inotispyctl binary

---
+++ bin/inotispyctl.c
@@ -0,0 +1,19 @@
+#include <zmq.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <json/json.h>
+
+void print_help (void);
+
+int
+main (int argv, char **argc)
+{
+    print_help();
+}
+
+void
+print_help (void)
+{
+    printf("\n** This is a future feature and has not been implemented yet **\n\n");
+}
