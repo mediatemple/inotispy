e6be249910b7551b8a6731082a99ca6162c36cf1
James Conerly <james.conerly@gmail.com>
2011-12-08 07:28:58 +0000

    * Working on init.d script for Redhat (Centos/sysv)

--- src/main.c
+++ src/main.c
@@ -30,6 +30,7 @@
 #include "zeromq.h"
 #include "config.h"
 #include "inotify.h"
+#include "utils.h"
 
 #include <time.h>
 #include <stdio.h>
@@ -42,8 +43,13 @@
 #include <getopt.h>
 #include <unistd.h>
 #include <pthread.h>
+#include <dirent.h>
 
 static pthread_mutex_t main_mutex = PTHREAD_MUTEX_INITIALIZER;
+
+/* Default location for our processes pid file. */
+#define DEFAULT_PID_FILE "/var/run/inotispy/inotispy.pid"
+static char *pid_file;
 
 /* This is the timer for dumping the rewatch roots, as well as
  * for checking to see if the configuration has been updated.
@@ -54,17 +60,21 @@ static time_t alarm_timer;
 static void print_help_and_exit(void);
 static void alarm_handler(void);
 static void sig_handler(int sig);
+static void write_pid(char *pidfile);
+static void check_pid(char *pidfile);
 
 int main(int argc, char **argv)
 {
-    int c, rv, inotify_fd;
+    int pid, c, rv, inotify_fd;
     void *zmq_receiver;
     struct utsname u_name;
     int option_index;
-    int silent, help;
+    int daemon_mode, silent, help;
     char *config_file;
 
     static struct option long_opts[] = {
+        {"daemon", no_argument, 0, 'd'},
+        {"pidfile", required_argument, 0, 'p'},
         {"silent", no_argument, 0, 's'},
         {"help", no_argument, 0, 'h'},
         {"config", required_argument, 0, 'c'},
@@ -86,20 +96,28 @@ int main(int argc, char **argv)
     }
 
     /* Handle command line args. */
+    daemon_mode = 0;
     silent = 0;
     help = 0;
+    pid_file = DEFAULT_PID_FILE;
     config_file = NULL;
     option_index = 0;
 
     while ((c =
-            getopt_long(argc, argv, "shc:", long_opts,
+            getopt_long(argc, argv, "dshc:p:", long_opts,
                         &option_index)) != -1) {
         switch (c) {
+        case 'd':
+            daemon_mode = 1;
+            break;
         case 's':
             silent = 1;
             break;
         case 'h':
             help = 1;
+            break;
+        case 'p':
+            pid_file = optarg;
             break;
         case 'c':
             config_file = optarg;
@@ -113,7 +131,7 @@ int main(int argc, char **argv)
             else
                 fprintf(stderr,
                         "Unknown option character `\\x%x'.\n", optopt);
-            return 1;
+            exit(1);
         default:
             abort();
         }
@@ -121,6 +139,18 @@ int main(int argc, char **argv)
 
     if (help)
         print_help_and_exit();
+
+    if (daemon_mode) {
+        check_pid(pid_file);
+
+        silent = 1;
+        if (daemon(0, 0) == -1) {
+            fprintf(stderr, "Failed to start inotispy in daemon mode: %s",
+                    strerror(errno));
+            exit(1);
+        }
+        write_pid(pid_file);
+    }
 
     /* Initialize our configuration. */
     init_config(silent, config_file);
@@ -136,7 +166,6 @@ int main(int argc, char **argv)
     /* Signal handling for graceful dying. */
     signal(SIGINT, sig_handler);
     signal(SIGSEGV, sig_handler);
-    signal(SIGALRM, sig_handler);
 
     log_notice("Initializing daemon");
 
@@ -183,6 +212,81 @@ int main(int argc, char **argv)
             zmq_handle_event();
         }
     }
+}
+
+static void check_pid(char *pidfile)
+{
+    DIR *d;
+    FILE *fp;
+    char line[8];
+    char *procdir;
+    int pid, rv;
+
+    /* Check to see if the pid file exists and if it has
+     * a value in it.
+     */
+    fp = fopen(pidfile, "r");
+    if (fp == NULL)
+        return;
+
+    if (fgets(line, sizeof line, fp) == NULL) {
+        if (!CONFIG->silent)
+            fprintf(stderr, "Found pid file %s but it was empty", pidfile);
+        fclose(fp);
+        return;
+    }
+
+    if (line[strlen(line) - 1] == '\n')
+        line[strlen(line) - 1] = '\0';
+
+    pid = atoi(line);
+    fclose(fp);
+
+    rv = mk_string(&procdir, "/proc/%d", pid);
+    if (rv == -1) {
+        fprintf(stderr,
+                "Failed to allocate memory while in check_pid()\n");
+        exit(1);
+    }
+
+    /* We have a pid file with a value, now let's check to see if
+     * this pid is currently running or is stale. If it's stale we
+     * remove it and let Inotispy run.
+     */
+    d = opendir(procdir);
+    free(procdir);
+
+    if (d == NULL) {
+        fprintf(stderr,
+                "Found stale pid file. Cleaning up and running Inotispy\n");
+        rv = unlink(pidfile);
+        if (rv == -1) {
+            fprintf(stderr, "Failed to unlink stale pid file %s: %s\n",
+                    pidfile, strerror(errno));
+            exit(1);
+        }
+        return;
+    }
+
+    closedir(d);
+    fprintf(stderr,
+            "Inotispy is currently running in daemon mode. Exiting...\n");
+    exit(1);
+}
+
+static void write_pid(char *pidfile)
+{
+    FILE *fp;
+
+    fp = fopen(pidfile, "w");
+    if (fp == NULL) {
+        fprintf(stderr, "Failed to open pid file %s for writing: %s\n",
+                pidfile, strerror(errno));
+        exit(1);
+    }
+
+    fprintf(fp, "%d", getpid());
+    fclose(fp);
 }
 
 static void sig_handler(int sig)
